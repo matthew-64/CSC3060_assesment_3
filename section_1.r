e93f96a3eeda555327b81bb95f1c0a52c28eb592
40153557 <melliott21@qub.ac.uk>
2019-12-28 12:55:49 +0000

    1.3

--- section_1.r
+++ section_1.r
@@ -96,7 +96,7 @@ correctness_cutoff_df <- data.frame(cut_off = p_data, correctness = correct_data
 # find largest correctness and corresponding cut off value
 max_correctness_pos <- 0
 max_correctness <- 0
-for (i in 1:nrow(blah)) {
+for (i in 1:nrow(correctness_cutoff_df)) {
   if (correctness_cutoff_df[i, "correctness"] > max_correctness) {
     max_correctness <- correctness_cutoff_df[i, "correctness"]
     max_correctness_pos <- i
@@ -127,13 +127,13 @@ print("##################  1.3 #########################")
 # create data frame with living and non-living categories
 catorgised_data <- DATA
 catorgised_data$catagory <- "non-living"
-catorgised_data$catagory[nr_pix_graph_data$label %in% living_labels] <- "living"
+catorgised_data$catagory[catorgised_data$label %in% living_labels] <- "living"
 
 
-# nr_pix graph
+# height graph
 height_graph <- ggplot(catorgised_data, aes(x = height, fill = as.factor(catagory))) +
   geom_density(adjust = 1.5, alpha = 0.5) + 
-  ggtitle("Density plot of\nNumber of pixels for Living and Non-living Images") +
+  ggtitle("Density plot of:\nHeight for Living and Non-living Images") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(fill=c("Living status"))
 
@@ -143,7 +143,7 @@ print(height_graph)
 # width graph
 width_graph <- ggplot(catorgised_data, aes(x = width, fill = as.factor(catagory))) +
   geom_density(adjust = 1.2, alpha = 0.5) + 
-  ggtitle("Density plot of\nWidth for Living and Non-living Images") +
+  ggtitle("Density plot of:\nWidth for Living and Non-living Images") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(fill=c("Living status"))
 
@@ -153,7 +153,7 @@ print(width_graph)
 # span graph
 span_graph <- ggplot(catorgised_data, aes(x = height, fill = as.factor(catagory))) +
   geom_density(adjust = 1.5, alpha = 0.5) + 
-  ggtitle("Density plot of\nSpan for Living and Non-living Images") +
+  ggtitle("Density plot of:\nSpan for Living and Non-living Images") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(fill=c("Living status"))
 
@@ -163,15 +163,15 @@ print(span_graph)
 # create fresh data set and shuffle
 data_shuffled <- DATA
 data_shuffled$living_status <- 0
-data_shuffled$living_status[nr_pix_graph_data$label %in% living_labels] <- 1
+data_shuffled$living_status[catorgised_data$label %in% living_labels] <- 1
 data_shuffled = data_shuffled[sample(nrow(data_shuffled)),]
 
 # build 3-way logistic regression model
-glmfit_nr_pix <- glm(living_status ~ height + width + span, 
-                     data = data_shuffled, 
-                     family = 'binomial')
+glmfit_3_way <- glm(living_status ~ height + width + span, 
+                    data = data_shuffled, 
+                    family = 'binomial')
 print("1.3: Linear regression for nr_pix, width and span:")
-print(summary(glmfit_nr_pix))
+print(summary(glmfit_3_way))
 
 
 # set up 5-fold crossvalidation
@@ -182,24 +182,90 @@ data_shuffled$folds <- cut(seq(1:nrow(data_shuffled)),
                            breaks=NUM_K_FOLDS,
                            labels=FALSE)
 
-correctectness_values <- c()
-fold_num <- c()
-# preform 5-fold crossvalidation
+# preform 5-fold crossvalidation with different cutoff points to find
+# the cut off value that provides the highest accuray
+cut_off_values <- c() # to record cut off that gives highest correctness
+correctness_values_for_each_cut_off <- c() # to record correctness for each cut off value
+correctectness_values <- c() # to store correctness for each fold
+
+for (x in 1:100) {
+  cut_off_value <- x/100
+  cut_off_values <- append(cut_off_values, cut_off_value)
+  
+  # preform cross validation for particular cut off value
+  for (i in 1:NUM_K_FOLDS) {
+    # create training and validation data for particular fold
+    train_items_this_fold  = data_shuffled[data_shuffled$folds != i,] 
+    validation_items_this_fold = data_shuffled[data_shuffled$folds == i,]
+    
+    # fit linear regression model on this fold
+    this_glmfit <- glm(living_status ~ height + width + span, 
+                       data = train_items_this_fold, 
+                       family = 'binomial')
+    
+    # validate current linear regression model
+    validation_items_this_fold$prediction = predict(this_glmfit, validation_items_this_fold, type="response")
+    validation_items_this_fold$predicted_correct = 0
+    validation_items_this_fold$predicted_correct[validation_items_this_fold$prediction > cut_off_value] = 1
+    
+    # calculate correctness
+    correct_items = validation_items_this_fold$predicted_correct == validation_items_this_fold$living_status
+    num_correct = nrow(validation_items_this_fold[correct_items,])
+    proportion_correct = num_correct / nrow(validation_items_this_fold)
+    
+    # recoed correctness with corresponding number of folds
+    correctectness_values <- append(correctectness_values, proportion_correct)
+    #fold_num <- append(fold_num, i)
+  }
+  # record mean correctness value over all folds
+  correctness_values_for_each_cut_off <- append(correctness_values_for_each_cut_off, mean(correctectness_values))
+  correctectness_values <- c()
+  
+}
+correctness_cutoff_df <- data.frame(cut_off = cut_off_values, correctness = correctness_values_for_each_cut_off)
+
+# find largest correctness and corresponding cut off value
+max_correctness_pos <- 0
+max_correctness <- 0
+for (i in 1:nrow(correctness_cutoff_df)) {
+  if (correctness_cutoff_df[i, "correctness"] > max_correctness) {
+    max_correctness <- correctness_cutoff_df[i, "correctness"]
+    max_correctness_pos <- i
+  }
+}
+max_cutoff_value <- correctness_cutoff_df[max_correctness_pos, "cut_off"]
+print(paste("1.3: max correctness = ", max_correctness, "at a cut-off value of ", max_cutoff_value))
+
+
+# Plot graph to visualise results of changing the cutoff value on correctness
+correctness_cut_off_graph <- ggplot(data = correctness_cutoff_df, aes(x = cut_off_values, y = correctness_values_for_each_cut_off)) +
+  geom_point() +
+  scale_y_continuous(name="Correctness") +
+  scale_x_continuous(name = "Cut Off Value") +
+  ggtitle("Correctness of Linear Regression Model Under Differing Cut Off Values") +
+  geom_hline(yintercept = max_correctness, linetype="dotted", color = "red", size=1) +
+  geom_vline(xintercept = max_cutoff_value, linetype="dotted", color = "red", size=1)
+print(correctness_cut_off_graph)
+
+
+# MAYBE TURN THIS INTO A FUNCTION
+#Preform k-fold cross validation with best suited cutoff point as calculated above
+fold_num <- c() # to store fold
 for (i in 1:NUM_K_FOLDS) {
   # create training and validation data for particular fold
   train_items_this_fold  = data_shuffled[data_shuffled$folds != i,] 
   validation_items_this_fold = data_shuffled[data_shuffled$folds == i,]
-
+  
   # fit linear regression model on this fold
   this_glmfit <- glm(living_status ~ height + width + span, 
                      data = train_items_this_fold, 
                      family = 'binomial')
-
+  
   # validate current linear regression model
   validation_items_this_fold$prediction = predict(this_glmfit, validation_items_this_fold, type="response")
   validation_items_this_fold$predicted_correct = 0
-  validation_items_this_fold$predicted_correct[validation_items_this_fold$prediction > 0.5] = 1
-
+  validation_items_this_fold$predicted_correct[validation_items_this_fold$prediction > max_cutoff_value] = 1
+  
   # calculate correctness
   correct_items = validation_items_this_fold$predicted_correct == validation_items_this_fold$living_status
   num_correct = nrow(validation_items_this_fold[correct_items,])
@@ -211,7 +277,8 @@ for (i in 1:NUM_K_FOLDS) {
 }
 
 # Understand corrrectness readings from 5-fold test
-print(paste("Average correctness of linear regression model measured from 5-fold test: ", mean(correctectness_values)))
+print(paste("Average correctness of linear regression model measured from 5-fold test:", mean(correctectness_values), 
+            ",using a cut-off value of:", max_cutoff_value))
 
 # Visualise correctness in a graph
 correctness_5_fold_df <- data.frame(fold_num, correctectness_values)
